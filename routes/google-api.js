7c4d85647b8603aea74558f92c15dccd096a2bee
Till Kundrun <tillkundrun1@yahoo.de>
2019-09-18 19:41:54 +0200

    remove branch unrelated file

--- routes/google-api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/google-api', (req, res, next) => {
-    res.render('google-api');
-});
-
-module.exports = router;
