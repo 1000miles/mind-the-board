30a91245452970c868dde5d7259488a330bf9854
Thuy Le <gitnap@mailbox.org>
2019-09-20 12:19:23 +0200

    semicolon

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Task = require('../models/Task')
+const Task = require('../models/Task');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
