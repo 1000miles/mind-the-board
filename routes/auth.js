cd0c48b9b1918313e1e84de35d74aed210661d47
Thuy Le <gitnap@mailbox.org>
2019-09-20 14:06:52 +0200

    reverts user role defaults

--- routes/auth.js
+++ routes/auth.js
@@ -39,12 +39,10 @@ router.post("/signup", (req, res, next) => {
 
     const salt = bcrypt.genSaltSync(bcryptSalt);
     const hashPass = bcrypt.hashSync(password, salt);
-    const role = "HR-Admin";
 
     const newUser = new User({
       username,
       password: hashPass,
-      role
     });
 
     newUser.save()
